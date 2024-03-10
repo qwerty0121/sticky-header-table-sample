b041c1b37499eaaad4e492e04038fe20e3026ea9
qwerty0121 <sh1ng0718o@gmail.com>
2024-03-10 17:58:10 +0900

    [add] <table>タグを利用した実装例を追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import GridTableView from "../views/GridTableView.vue";
 import SubGridTableView from "../views/SubGridTableView.vue";
+import TableView from "@/views/TableView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,14 @@ const router = createRouter({
       component: SubGridTableView,
       meta: {
         viewName: "SubGridTable",
+      },
+    },
+    {
+      path: "/table",
+      name: "table",
+      component: TableView,
+      meta: {
+        viewName: "Table",
       },
     },
   ],
---
+++ src/views/TableView.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { schema, items } from "@/repositories/items";
+</script>
+
+<template>
+  <div class="table-container">
+    <table class="table">
+      <thead>
+        <tr class="row row--header">
+          <template v-for="[key, { name }] in Object.entries(schema)">
+            <th class="cell cell--header" :class="`cell-${key}`">{{ name }}</th>
+          </template>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="item in items">
+          <tr class="row row--body">
+            <template v-for="key in Object.keys(schema)">
+              <td class="cell cell--body" :class="{ 'cell--warn': item.warn }">
+                {{ item[key] }}
+              </td>
+            </template>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.table-container {
+  font-size: 0.7em;
+  height: 350px;
+  overflow: auto;
+  position: relative;
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.row--header {
+  position: sticky;
+  top: 0;
+}
+
+.cell {
+  padding: 5px;
+}
+
+.cell--header {
+  background-color: blue;
+  color: white;
+}
+
+.cell--body {
+  background-color: lightgray;
+}
+
+.cell--warn {
+  background-color: #99ff99;
+}
+
+.cell-status,
+.cell-createDate,
+.cell-responseDate,
+.cell-deadlineDate,
+.cell-category {
+  min-width: 70px;
+}
+.cell-subject,
+.cell-memo,
+.cell-customerName,
+.cell-picName,
+.cell-branchPicName {
+  min-width: 150px;
+}
+</style>
